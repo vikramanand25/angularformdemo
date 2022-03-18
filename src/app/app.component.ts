4e7cd9af68ab586f6079303950c9eb9f7b269a24
vikramanand25 <vikramanand27@gmail.com>
2022-03-18 09:52:34 +0530

    all files added

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'formpage';
+  newUser:string="";
+  userinfo:any={};
+  getName(getname:string){
+    this.newUser=getname;
+  }
+  submitNewUserData(info:NgForm)
+  {
+    this.userinfo=info;
+    console.log(this.userinfo);
+  }
+}
